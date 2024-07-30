a88b35b110d15ec51b61d8aa0512e4601266f006
Tejashri Taral <160386036+Tejashri-Taral@users.noreply.github.com>
2024-07-30 20:07:24 +0530

    Create sort list.java

---
+++ Day-08/sort list.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode sortList(ListNode head) {
+        if(head==null || head.next==null){
+            return head;
+        }
+        PriorityQueue<Integer>pq=new PriorityQueue<>();
+        ListNode curr=head;
+        while(curr!=null){
+            pq.add(curr.val);
+            curr=curr.next;
+        }
+        
+        curr=head;
+        while(curr!=null && !pq.isEmpty()) {
+            curr.val=pq.poll();
+            curr=curr.next;
+        }
+        return head;
+    }
+}
