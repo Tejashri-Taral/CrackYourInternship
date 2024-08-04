3ab5556afe0dceb21d6bef93062a250c760e74a6
Tejashri Taral <160386036+Tejashri-Taral@users.noreply.github.com>
2024-08-04 19:27:11 +0530

    Create Path Sum.java

---
+++ Day-13/Path Sum.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        return pathSum(root,0,targetSum);
+    }
+    boolean pathSum(TreeNode root,int sum,int targetSum) {
+        if(root==null) {
+            return false;
+        }
+        if(root.left==null && root.right==null) {
+            sum+=root.val;
+            if(sum==targetSum) {
+                return true;
+            }
+        }
+        return pathSum(root.left,sum+root.val,targetSum) || pathSum(root.right,sum+root.val,targetSum);
+    }
+}
