cadc5f3500955dcdbe9e93cbe5003338c03ebc5e
Tejashri Taral <160386036+Tejashri-Taral@users.noreply.github.com>
2024-08-09 22:20:39 +0530

    Create Implement Queue using Stacks.java

---
+++ Day-18/Implement Queue using Stacks.java
@@ -0,0 +1,65 @@
+class MyQueue {
+    private Stack<Integer> stack1; // Main stack for enqueue operation
+    private Stack<Integer> stack2; // Auxiliary stack for dequeue operation
+
+    public MyQueue() {
+        stack1 = new Stack<>();
+        stack2 = new Stack<>();
+    }
+
+    public void push(int x) {
+        stack1.push(x);
+    }
+
+    public int pop() {
+        if (empty()) {
+            System.out.println("Queue is empty. Cannot dequeue.");
+            return -1;
+        }
+
+        if (stack2.isEmpty()) {
+            while (!stack1.isEmpty()) {
+                stack2.push(stack1.pop());
+            }
+        }
+
+        return stack2.pop();
+    }
+
+    public int peek() {
+        if (empty()) {
+            System.out.println("Queue is empty. No front element.");
+            return -1;
+        }
+
+        if (stack2.isEmpty()) {
+            while (!stack1.isEmpty()) {
+                stack2.push(stack1.pop());
+            }
+        }
+
+        return stack2.peek();
+    }
+
+    public boolean empty() {
+        return stack1.isEmpty() && stack2.isEmpty();
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
